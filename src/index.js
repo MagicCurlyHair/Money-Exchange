ec6acfe0371bf8660fa9a64e15d535d1e65a12cc
MagicCurlyHair <g.shokanov@gmail.com>
2018-09-23 22:23:39 +0300

    MakeExchange function is working correctly now

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    // return statements according to specification
+    if (currency <= 0){
+        return {};
+    }
+    if (currency > 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    let newCurr = currency;
+    let change = {};
+    let counter;
+    let cents = ["H", "Q", "D", "N", "P"];
+    let coin;
+    let value;
+    // iterate over possible coin values
+    for (let i = 0; i < cents.length; i++){
+        coin = cents[i];
+        // assign corresponding coin value
+        switch(coin){
+            case "H":
+                value = 50;
+                break;
+            case "Q":
+                value = 25;
+                break;
+            case "D":
+                value = 10;
+                break;
+            case "N":
+                value = 5;
+                break;
+            case "P":
+                value = 1;
+        }
+        counter = 0;
+        // count maximum amount of coins in currency
+        while (newCurr >= value){
+            newCurr -= value;
+            counter++;
+        }
+        // add current number of coins to change object
+        if (counter > 0){
+            change[coin] = counter;
+        }
+    }
+    return change;
 }
